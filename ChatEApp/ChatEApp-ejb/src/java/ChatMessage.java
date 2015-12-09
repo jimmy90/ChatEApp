4b3ba3db6c1cf3eb8be944db93380fc3a4812c3f
Jimmy2xx <Jimmy2xx@10.11.26.251>
2015-12-09 16:05:41 +0100

    Index html & css - ChatMessage

---
+++ ChatEApp/ChatEApp-ejb/src/java/ChatMessage.java
@@ -0,0 +1,44 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jimmy2xx
+ */
+public class ChatMessage {
+    
+    protected ChatMessage(String message, String emetteur, Date recu) {
+		super();
+		this.message = message;
+		this.emetteur = emetteur;
+		this.recu = recu;
+    }
+    private String message;
+    private String emetteur;
+    private Date recu;
+    public String getMessage() {
+            return message;
+    }
+    public void setMessage(String message) {
+            this.message = message;
+    }
+    public String getEmetteur() {
+            return emetteur;
+    }
+    public void setEmetteur(String emetteur) {
+            this.emetteur = emetteur;
+    }
+    public Date getRecu() {
+            return recu;
+    }
+    public void setRecu(Date recu) {
+            this.recu = recu;
+    }
+
+}
